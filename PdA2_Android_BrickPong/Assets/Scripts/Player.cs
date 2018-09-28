2926b9b8581de1c6264c4e544e2a91a8b9e1d668
Paskuihernandez <paskuihernandez@gmail.com>
2018-09-28 10:09:06 +0200

    dfg

--- PdA2_Android_BrickPong/Assets/Scripts/Player.cs
+++ PdA2_Android_BrickPong/Assets/Scripts/Player.cs
@@ -4,60 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    float speed;
 
-    private int finId1 = -1; //id finger for cancel touch event
-    private int finId2 = -1;
+    private Rigidbody2D characterBody;
 
-    public float moveSpeed = 300;
-    public GameObject player;
+    float VerticalInput;
 
-    private Rigidbody playerBody;
-    private float ScreenWidth;
+    // Use this for initialization
+    void Start () {
+        speed = 0.3f;
+        characterBody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 
-    void Start()
+    public void LUPTouch()
     {
-        ScreenWidth = Screen.width;
-        playerBody = player.GetComponent<Rigidbody>();
-        Input.multiTouchEnabled = true; //enabled Multitouch
+        Debug.Log("LUP");
+        characterBody.AddForce(new Vector2(VerticalInput * speed * Time.deltaTime, 0));
+
+    }
+    public void LDownTouch()
+    {
+        Debug.Log("LDown");
+        transform.Translate(0, -speed, 0);
+    }
+    public void RUPTouch()
+    {
+        Debug.Log("RUP");
+    }
+    public void RDownTouch()
+    {
+        Debug.Log("RDown");
     }
 
-    void Update()
-    {
 
-        //First check count of touch
-        if (Input.touchCount > 0)
-        {
-            foreach (Touch touch in Input.touches)
-            {
-                //For left half screen
-                if (touch.phase == TouchPhase.Began && touch.position.x <= Screen.width && finId1 == -1)
-                {
-                    RunCharacter(1.0f);
-                    //Do something: start other function
-                    finId1 = touch.fingerId; //store Id finger
-                }
-                //For right half screen
-                if (touch.phase == TouchPhase.Began && touch.position.x > Screen.width && finId2 == -1)
-                {
-                    //Do something
-                    finId2 = touch.fingerId;
-                }
-                if (touch.phase == TouchPhase.Ended)
-                { //correct end of touch
-                    if (touch.fingerId == finId1)
-                    { //check id finger for end touch
-                        finId1 = -1;
-                    }
-                    else if (touch.fingerId == finId2)
-                    {
-                        finId2 = -1;
-                    }
-                }
-            }
-        }
-    }
-    private void RunCharacter(float horizontalInput)
-    {
-        playerBody.AddForce(new Vector2(horizontalInput * moveSpeed * Time.deltaTime, 0));
-    }
 }
