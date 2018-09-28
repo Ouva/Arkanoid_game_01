d688394cc017ce373bff7c43873a263e0dfeb396
Paskuihernandez <paskuihernandez@gmail.com>
2018-09-28 09:01:23 +0200

    28/9

--- PdA2_Android_BrickPong/Assets/Scripts/Player.cs
+++ PdA2_Android_BrickPong/Assets/Scripts/Player.cs
@@ -4,25 +4,60 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public KeyCode up;
-    public KeyCode down;
 
-    float speed;
+    private int finId1 = -1; //id finger for cancel touch event
+    private int finId2 = -1;
 
-	// Use this for initialization
-	void Start () {
-        speed = 0.3f;
-	}
-	
-	// Update is called once per frame
-	/*void Update () {
-        if ()
+    public float moveSpeed = 300;
+    public GameObject player;
+
+    private Rigidbody playerBody;
+    private float ScreenWidth;
+
+    void Start()
+    {
+        ScreenWidth = Screen.width;
+        playerBody = player.GetComponent<Rigidbody>();
+        Input.multiTouchEnabled = true; //enabled Multitouch
+    }
+
+    void Update()
+    {
+
+        //First check count of touch
+        if (Input.touchCount > 0)
         {
-            transform.Translate(0, 1, 0);
+            foreach (Touch touch in Input.touches)
+            {
+                //For left half screen
+                if (touch.phase == TouchPhase.Began && touch.position.x <= Screen.width && finId1 == -1)
+                {
+                    RunCharacter(1.0f);
+                    //Do something: start other function
+                    finId1 = touch.fingerId; //store Id finger
+                }
+                //For right half screen
+                if (touch.phase == TouchPhase.Began && touch.position.x > Screen.width && finId2 == -1)
+                {
+                    //Do something
+                    finId2 = touch.fingerId;
+                }
+                if (touch.phase == TouchPhase.Ended)
+                { //correct end of touch
+                    if (touch.fingerId == finId1)
+                    { //check id finger for end touch
+                        finId1 = -1;
+                    }
+                    else if (touch.fingerId == finId2)
+                    {
+                        finId2 = -1;
+                    }
+                }
+            }
         }
-        if ()
-        {
-            transform.Translate(0, -1, 0);
-        }
-    }*/
+    }
+    private void RunCharacter(float horizontalInput)
+    {
+        playerBody.AddForce(new Vector2(horizontalInput * moveSpeed * Time.deltaTime, 0));
+    }
 }
